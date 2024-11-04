0e9e58176ca88cab2edcbac121e28d8d6c4944af
Blacknibolion <35793362+blacknibolion@users.noreply.github.com>
2024-11-05 00:09:10 +0100

    update

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -12,20 +12,42 @@ Console.WriteLine($"{firstName} ate {amountOfFoodAte} types of food only today!"
   consist of a letter from A to E, and a three
   digit number. Ex. A123.
 */
-Random random = new Random();
+/*Random random = new Random();
 string [] orderIDs = new string[5];
 
 for(int i = 0; i < orderIDs.Length; i++){
 
-int prefixValue = random.Next(65, 70);
+    int prefixValue = random.Next(65, 70);
 
-string prefix = Convert.ToChar(prefixValue).ToString();
-string suffix = random.Next(1, 1000).ToString("000");
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
 
-orderIDs[i] = prefix + suffix;
+    orderIDs[i] = prefix + suffix;
 }
 
 foreach (var orderID in orderIDs)
 {
     Console.WriteLine(orderID);
+}*/
+
+//The high-level purpose of this code is to reverse a string 
+//and count the number of times a particular character appears.
+string message = "The quick brown fox jumps over the lazy dog.";
+
+char[] newMessage = message.ToCharArray();
+
+Array.Reverse(newMessage);
+int countLetter = 0;
+
+foreach (char character in newMessage){
+  if( character == 'o'){
+    countLetter ++;
+    }
+  
+
 }
+// convert back to string
+  string updatedMessage = new string(newMessage);
+  //print it out
+  Console.WriteLine(updatedMessage);
+  Console.WriteLine($"'o' appears {countLetter} times");
